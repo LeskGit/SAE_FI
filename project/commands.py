fdf1fa573c98cf4b2789e1bd38a2b5ee6e823bf4
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-10-24 12:08:22 +0200

    Ajout du test en commentaire

--- project/commands.py
+++ project/commands.py
@@ -6,9 +6,21 @@ def syncdb():
     '''Creates the tables and populates them with data.'''
     db.create_all()
 
-    from .models import TriggerManager
+    from .models import TriggerManager, User
     TriggerManager()
 
+    #usr = User(num_tel = '0123456789',
+    #            nom = 'Doe',
+    #            prenom = 'John',
+    #            password = 'password',
+    #            adresse = '1 rue de la Paix',
+    #            email = 'a@b.c',
+    #            blacklisted = False,
+    #            points_fidelite = 0,
+    #            prix_panier = 0)
+    
+    #db.session.add(usr)
+    #db.session.commit()
 
     #TODO : Not implemented yet
 
