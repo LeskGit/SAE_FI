f24acab3d6ba5f2126a3cc35262b2731e48d5d77
Loris-Gr <grandchamploris@gmail.com>
2024-11-04 16:44:16 +0100

    changement noms variables et fonctions

--- project/views/authentification.py
+++ project/views/authentification.py
@@ -6,20 +6,20 @@ from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
 from wtforms import StringField, PasswordField, EmailField
 
-class LoginForm (FlaskForm) :
-    num_tel = StringField("Numero_telephone")
+class LoginForm (FlaskForm):
+    phone_number = StringField("Numero_telephone")
     password = PasswordField("Mot_de_passe")
 
-class RegisterForm ( FlaskForm ) :
-    num_tel = StringField("Numero_telephone")
-    nom = StringField("Nom")
-    prenom = StringField("Prenom")
+class RegisterForm (FlaskForm):
+    phone_number = StringField("Numero_telephone")
+    name = StringField("Nom")
+    first_name = StringField("Prenom")
     password = PasswordField("Mot_de_passe")
-    adresse = StringField("Adresse")
+    addresse = StringField("Adresse")
     email = EmailField("Email")
     repcatcha = RecaptchaField()
 
-    def get_user_authentifie(self):
+    def get_authentificated_user(self):
         """permet de savoir si le mot de passe de 
         l'utilisateur est bon
 
@@ -29,40 +29,40 @@ class RegisterForm ( FlaskForm ) :
         user = User.query.get(self.num_tel.data)
         if user is None:
             return None
-        m = sha256 ()
+        m = sha256()
         m.update(self.password.data.encode())
-        passwd = m.hexdigest ()
+        passwd = m.hexdigest()
         return user if passwd == user.password else None
 
 @app.route("/connexion", methods = ("GET", "POST", ))
-def connexion() :
+def login():
     f = LoginForm()
-    if f.validate_on_submit ():
-        user = f.get_authenticated_user ()
-        if user :
+    if f.validate_on_submit():
+        user = f.get_authenticated_user()
+        if user:
             login_user(user)
             return render_template("profil_client_connecte.html", client = user) 
     return render_template("connexion.html", form = f)
 
 @app.route("/deconnexion")
-def logout ():
-    logout_user ()
-    return redirect ( url_for ("accueil"))
+def logout():
+    logout_user()
+    return redirect(url_for("accueil"))
 
 @app.route("/inscription")
-def inscription() :
+def register():
     f = RegisterForm()
     if f.validate_on_submit():
-        numero_tel = f.num_tel.data
-        password = f.password.data
-        nom_c = f.nom.data
-        prenom_c = f.prenom.data
-        adresse_c = f.adresse.data
-        email_c = f.email.data
+        phone = f.phone_number.data
+        passwd = f.password.data
+        name = f.name.data
+        first_name = f.first_name.data
+        addresse = f.addresse.data
+        mail = f.email.data
         m = sha256()
-        m.update(password.encode())
-        u = User(num_tel=numero_tel, password=m.hexdigest(), nom = nom_c, prenom = prenom_c, adresse = adresse_c, email = email_c)
+        m.update(passwd.encode())
+        u = User(num_tel=phone, password=m.hexdigest(), nom = name, prenom = first_name, adresse = addresse, email = mail)
         db.session.add(u)
         db.session.commit()
-        return redirect(url_for("connexion"))
+        return redirect(url_for("login"))
     return render_template("inscription.html", form = f)
