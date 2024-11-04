13fdb5fc213a49e0e727e6279407bfe4fff2f31e
CorentinThu <corentin.thuault@etu.univ-orleans.fr>
2024-11-04 10:44:07 +0100

    Resolution des problemes d'arborescence

--- project/commands.py
+++ project/commands.py
@@ -22,3 +22,29 @@ def dropdb():
     '''Drops the tables.'''
 
     db.drop_all()
+
+@app.cli.command()
+@click.argument ("username")
+@click.argument ("password")
+def newuser (username , password ):
+    """Adds a new user. """
+    from.models import User
+    from hashlib import sha256
+    m = sha256 ()
+    m.update( password.encode())
+    u = User( username =username , password =m.hexdigest ())
+    db.session.add(u)
+    db.session.commit()
+
+@app.cli.command()
+@click.argument ("username")
+@click.argument ("password")
+def newpassword (username, password ):
+    """ Change the actual user password. """
+    from.models import User
+    from hashlib import sha256
+    m = sha256 ()
+    m.update( password.encode())
+    u = User.query.get(username)
+    u.password = m.hexdigest ()
+    db.session.commit()
