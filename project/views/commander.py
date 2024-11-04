7c37112191807b8bdccbc75e7738af66b1e317ec
Loris-Gr <grandchamploris@gmail.com>
2024-11-04 11:17:32 +0100

    ajout panier, choix_paiement, paiement

--- project/views/commander.py
+++ project/views/commander.py
@@ -1,6 +1,6 @@
 from .app import app, db
 from flask import render_template, url_for, redirect, request
-#from .models import 
+#from .models import
 from flask_wtf import FlaskForm
 from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
@@ -12,3 +12,16 @@ def commander() :
 @app.route("/commander_plat", methods = ("POST",))
 def ajout_plat() :
     ...
+
+@app.route("/panier")
+def panier() :
+    return render_template("panier.html")
+
+@app.route("/choix_paiement")
+def choix_paiement() :
+    return render_template("choix_paiement.html")
+
+@app.route("/paiement")
+def paiement_cb():
+    f = ...
+    return render_template("paiement_cb.html", form=f)
