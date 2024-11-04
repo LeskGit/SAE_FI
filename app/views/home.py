c63ec80021f2331396382350cb01b17eb3dd9fc6
Loris-Gr <grandchamploris@gmail.com>
2024-11-04 10:19:56 +0100

    création route home, nouveaute, a_propos, contact

--- app/views/home.py
+++ app/views/home.py
@@ -4,3 +4,19 @@ from flask import render_template, url_for, redirect, request
 from flask_wtf import FlaskForm
 from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
+
+@app.route("/")
+def home() :
+    return render_template("home.html")
+
+@app.route("/a_propos")
+def a_propos() :
+    return render_template("a_propos.html")
+
+@app.route("/nouveaute")
+def nouveaute() :
+    return render_template("nouveaute.html")
+
+@app.route("/contact")
+def contact() :
+    return render_template("contact.html")
