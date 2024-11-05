53fc26b5e207150711290f14cf5494bd6b50f1e8
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-11-05 15:20:17 +0100

    Ajout de la page contact

--- project/views/home_views.py
+++ project/views/home_views.py
@@ -1,3 +1,4 @@
+from wtforms import EmailField, StringField, TextAreaField
 from project import app, db
 from flask import render_template, url_for, redirect, request
 #from .models import 
@@ -5,18 +6,24 @@ from flask_wtf import FlaskForm
 from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
 
+class ContactForm(FlaskForm):
+    email = EmailField('Email')
+    objet = StringField('Objet')
+    message = TextAreaField('Message')
+
 @app.route("/")
-def home() :
+def home():
     return render_template("home.html")
 
 @app.route("/a_propos")
-def a_propos() :
+def a_propos():
     return render_template("a_propos.html")
 
 @app.route("/nouveaute")
-def nouveaute() :
+def nouveaute():
     return render_template("nouveaute.html")
 
 @app.route("/contact")
-def contact() :
-    return render_template("contact.html")
+def contact():
+    f = ContactForm()
+    return render_template("contact.html", form=f)
