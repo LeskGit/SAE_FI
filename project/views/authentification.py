0d145b8bad086a17a8df19e24e7175b954536774
Loris-Gr <grandchamploris@gmail.com>
2024-11-05 09:06:50 +0100

    correction connexion pour tester

--- project/views/authentification.py
+++ project/views/authentification.py
@@ -1,6 +1,5 @@
 from project import app, db
 from flask import render_template, url_for, redirect, request
-from models import User
 from flask_wtf import FlaskForm, RecaptchaField
 from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
