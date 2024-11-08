4346607aa8f994f7d72ad602c9ae13e38aa9aece
CorentinThu <corentin.thuault@etu.univ-orleans.fr>
2024-11-08 10:10:33 +0100

    Ajout de la gestion d'erreur pour l'inscription

--- project/views/authentification.py
+++ project/views/authentification.py
@@ -4,12 +4,12 @@ from flask_wtf import FlaskForm, RecaptchaField
 from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
 from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo, Email, Length, Regexp
+from wtforms.validators import DataRequired, EqualTo, Email, Length, Regexp, ValidationError
 from project.models import User
 
 class LoginForm (FlaskForm):
     phone_number = StringField("Numéro de téléphone", validators=[DataRequired(), 
-                                                                  Length(min=10, max=10, message = 'Longueur incorrect'), 
+                                                                  Length(min=10, max=10, message = 'Longueur incorrecte.'), 
                                                                   Regexp(r'^\d{10}$', message="Le numéro de téléphone invalide.")])
     password = PasswordField("Mot de passe", validators=[DataRequired(), Length(max=64)])
 
@@ -30,21 +30,35 @@ class LoginForm (FlaskForm):
 
 class RegisterForm (FlaskForm):
     phone_number = StringField("Numéro téléphone", validators=[DataRequired(), 
-                                                               Length(min=10, max=10, message = 'Longueur incorrect'),
-                                                               Regexp(r'^\d{10}$', message="Le numéro de téléphone invalide.")])
+                                                               Length(min=10, max=10, message = 'Longueur incorrecte.'),
+                                                               Regexp(r'^\d{10}$', message="Le numéro de téléphone est invalide.")])
     name = StringField("Nom", validators=[DataRequired(), 
                                           Length(max=32)])
+                                          
     first_name = StringField("Prénom", validators=[DataRequired(), 
                                                    Length(max=32)])
+
+    email = EmailField("Email", validators=[DataRequired(), Email(message='Adresse mail invalide.'), 
+                                            Length(max=64)])
+
+    address = StringField("Adresse", validators=[DataRequired(), Length(max=64)])
+
     password = PasswordField("Mot de passe", validators=[DataRequired(), 
                                                          Length(max=64)])
+
     password_check = PasswordField("Confirmez votre mot de passe", validators=[DataRequired(), 
-                                                                               EqualTo('password', message='Les mots de passe doivent correspondre'),])
-    address = StringField("Adresse", validators=[DataRequired(), Length(max=64)])
-    email = EmailField("Email", validators=[DataRequired(), Email(message='addresse mail invalide'), 
-                                            Length(max=64)])
+                                                                               EqualTo('password', message='Les mots de passe doivent correspondre.'),])
+
     # Commentaire de la ligne en-dessous à enlever une fois le captcha mis en place 
     #recaptcha = RecaptchaField() 
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("Cet e-mail est déjà utilisé.")
+
+    def validate_phone_number(self, field):
+        if User.query.filter_by(num_tel=field.data).first():
+            raise ValidationError("Ce numéro de téléphone est déjà utilisé.")
 
     def get_authentificated_user(self):
         """permet de savoir si le mot de passe de 
@@ -80,7 +94,7 @@ def login():
         if the_user:
             login_user(the_user)
             return redirect(url_for("home"))
-        return render_template("connexion.html", form = f, error = 'incorrect_password')
+        return render_template("connexion.html", form = f, error = 'Mot de passe incorrect.')
     return render_template("connexion.html", form = f)
 
 @app.route("/deconnexion")
@@ -92,11 +106,6 @@ def logout():
 def register():
     f = RegisterForm()
     if f.validate_on_submit():
-        passwd = f.password.data
-        passwd_2 = f.password_check.data
-        if passwd != passwd_2 :
-            print("mot de passe pas bon")
-            return render_template("inscription.html", form = f)
         u = f.create_user()
         if User.query.get(u.get_id()) :
             return render_template("inscription.html", form = f)
