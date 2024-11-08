9678e0238baa93c16cf54bd239e32b6718427b09
CorentinThu <corentin.thuault@etu.univ-orleans.fr>
2024-11-08 12:17:11 +0100

    Ajout d'un système d'admin

--- project/models.py
+++ project/models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):
         unique=True
     )
     blackliste = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default = False)
     points_fidelite = db.Column(db.Integer, default=0)
     prix_panier = db.Column(db.Float, default=0)
     les_commandes = db.relationship("Commandes", back_populates = "les_clients")
--- project/views/administration.py
+++ project/views/administration.py
@@ -1,34 +1,52 @@
 from project import app, db
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, flash
 #from .models import
 from flask_wtf import FlaskForm
 from flask_login import login_user , current_user, logout_user, login_required
 from hashlib import sha256
+from functools import wraps
+
+def admin_required(f):
+    @wraps(f)
+    @login_required
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin:
+            flash("Vous devez être administrateur pour accéder à cette page.", "danger")
+            return redirect(url_for("home"))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/admin")
+@admin_required
 def admin():
     return render_template("admin_traiteur.html")
 
 @app.route("/suivi/commande")
+@admin_required
 def suivi_commande() :
     return render_template("suivi_commandes.html")
 
 @app.route("/suivi/stock")
+@admin_required
 def suivi_stock() :
     return render_template("suivi_stock.html")
 
 @app.route("/creation/plat")
+@admin_required
 def creation_plat():
     return render_template("creation_plat.html")
 
 @app.route("/creation/offre")
+@admin_required
 def creation_offre():
     return render_template("creation_offre.html")
 
 @app.route("/edition/plat")
+@admin_required
 def edition_plat():
     return render_template("edition_plat.html")
 
 @app.route("/edition/offre")
+@admin_required
 def edition_offre():
     return render_template("edition_offre.html")
